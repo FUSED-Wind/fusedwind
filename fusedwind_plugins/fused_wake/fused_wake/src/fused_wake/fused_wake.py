1d37a8a8f7dd90e58552dcabc402c14508cda35a
Pierre-Elouan Réthoré <pire@dtu.dk>
2013-09-17 12:08:16 +0200

    adding the fused_wake plugin

---
+++ fusedwind_plugins/fused_wake/fused_wake/src/fused_wake/fused_wake.py
@@ -0,0 +1,21 @@
+#__all__ = ['fused_wake']
+
+# from openmdao.main.api import Component
+# from openmdao.lib.datatypes.api import Float
+
+
+
+# # Make sure that your class has some kind of docstring. Otherwise
+# # the descriptions for your variables won't show up in the
+# # source ducumentation.
+# class fused_wake(Component):
+#     """
+#     """
+#     # declare inputs and outputs here, for example:
+#     #x = Float(0.0, iotype='in', desc='description for x')
+#     #y = Float(0.0, iotype='out', desc='description for y')
+
+#     def execute(self):
+#         """ do your calculations here """
+#         pass
+#         
